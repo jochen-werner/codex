16c032fc46fecb84c6d4499a20b1f54629bfdaa9
Shea Lewis <shea.lewis89@gmail.com>
2015-07-13 22:12:42 -0700

    Little bit of styling

--- src/resources/views/layouts/codex.blade.php
+++ src/resources/views/layouts/codex.blade.php
@@ -10,8 +10,24 @@
 	<!-- Bootstrap -->
 	<link rel="stylesheet" type="text/css" href="/vendor/codex/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="/vendor/codex/css/paper.min.css">
+	<link rel="stylesheet" type="text/css" href="/vendor/codex/css/codex.css">
 </head>
 <body>
+	<nav class="navbar navbar-inverse navbar-fixed-top">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+
+				<a class="navbar-brand" href="/codex">Codex</a>
+			</div>
+		</div>
+	</nav>
+
 	<div class="container">
 		@yield('content')
 	</div>
