91474db263edb33b785f44bb73a4b9b936526657
kaidesu <shea.lewis89@gmail.com>
2015-07-28 16:48:23 -0700

    Convert spaces to tabs

--- src/Codex/Codex/Parsers/ParsedownExtra.php
+++ src/Codex/Codex/Parsers/ParsedownExtra.php
@@ -5,143 +5,143 @@ use ParsedownExtra as BaseParsedownExtra;
 
 class ParsedownExtra extends BaseParsedownExtra
 {
-    /**
-     * Parse fenced code blocks.
-     *
-     * @param  array  $line
-     * @return array
-     */
+	/**
+	 * Parse fenced code blocks.
+	 *
+	 * @param  array  $line
+	 * @return array
+	 */
 	protected function blockFencedCode($line)
-    {
-        $regex = '/^(['.$line['text'][0].']{3,})[ ]*([\w-]+)?[ ]*$/';
+	{
+		$regex = '/^(['.$line['text'][0].']{3,})[ ]*([\w-]+)?[ ]*$/';
 
-        if (preg_match($regex, $line['text'], $matches)) {
-            $element = [
-                'name' => 'code',
-                'text' => '',
-            ];
+		if (preg_match($regex, $line['text'], $matches)) {
+			$element = [
+				'name' => 'code',
+				'text' => '',
+			];
 
-            if (isset($matches[2])) {
-                $class = 'prettyprint lang-'.$matches[2];
+			if (isset($matches[2])) {
+				$class = 'prettyprint lang-'.$matches[2];
 
-                $element['attributes'] = ['class' => $class];
-            }
+				$element['attributes'] = ['class' => $class];
+			}
 
-            $block = [
-                'char'    => $line['text'][0],
-                'element' => [
-                    'name'    => 'pre',
-                    'handler' => 'element',
-                    'text'    => $element,
-                ],
-            ];
+			$block = [
+				'char'    => $line['text'][0],
+				'element' => [
+					'name'    => 'pre',
+					'handler' => 'element',
+					'text'    => $element,
+				],
+			];
 
-            return $block;
-        }
-    }
+			return $block;
+		}
+	}
 
-    /**
-     * Parse tables.
-     *
-     * @param  array  $line
-     * @param  array  $block
-     * @return array
-     */
-    protected function blockTable($line, array $block = null)
-    {
-        if ( ! isset($block) or isset($block['type']) or isset($block['interrupted'])) {
-            return;
-        }
+	/**
+	 * Parse tables.
+	 *
+	 * @param  array  $line
+	 * @param  array  $block
+	 * @return array
+	 */
+	protected function blockTable($line, array $block = null)
+	{
+		if ( ! isset($block) or isset($block['type']) or isset($block['interrupted'])) {
+			return;
+		}
 
-        if (strpos($block['element']['text'], '|') !== false and chop($line['text'], ' -:|') === '') {
-            $alignments = array();
+		if (strpos($block['element']['text'], '|') !== false and chop($line['text'], ' -:|') === '') {
+			$alignments = array();
 
-            $divider = $line['text'];
-            $divider = trim($divider);
-            $divider = trim($divider, '|');
+			$divider = $line['text'];
+			$divider = trim($divider);
+			$divider = trim($divider, '|');
 
-            $dividerCells = explode('|', $divider);
+			$dividerCells = explode('|', $divider);
 
-            foreach ($dividerCells as $dividerCell) {
-                $dividerCell = trim($dividerCell);
+			foreach ($dividerCells as $dividerCell) {
+				$dividerCell = trim($dividerCell);
 
-                if ($dividerCell === '') {
-                    continue;
-                }
+				if ($dividerCell === '') {
+					continue;
+				}
 
-                $alignment = null;
+				$alignment = null;
 
-                if ($dividerCell[0] === ':') {
-                    $alignment = 'left';
-                }
+				if ($dividerCell[0] === ':') {
+					$alignment = 'left';
+				}
 
-                if (substr($dividerCell, - 1) === ':') {
-                    $alignment = $alignment === 'left' ? 'center' : 'right';
-                }
+				if (substr($dividerCell, - 1) === ':') {
+					$alignment = $alignment === 'left' ? 'center' : 'right';
+				}
 
-                $alignments[] = $alignment;
-            }
+				$alignments[] = $alignment;
+			}
 
-            $headerElements = array();
+			$headerElements = array();
 
-            $header = $block['element']['text'];
+			$header = $block['element']['text'];
 
-            $header = trim($header);
-            $header = trim($header, '|');
+			$header = trim($header);
+			$header = trim($header, '|');
 
-            $headerCells = explode('|', $header);
+			$headerCells = explode('|', $header);
 
-            foreach ($headerCells as $index => $headerCell)
-            {
-                $headerCell = trim($headerCell);
+			foreach ($headerCells as $index => $headerCell)
+			{
+				$headerCell = trim($headerCell);
 
-                $headerElement = [
-                    'name'    => 'th',
-                    'text'    => $headerCell,
-                    'handler' => 'line',
-                ];
+				$headerElement = [
+					'name'    => 'th',
+					'text'    => $headerCell,
+					'handler' => 'line',
+				];
 
-                if (isset($alignments[$index])) {
-                    $alignment = $alignments[$index];
+				if (isset($alignments[$index])) {
+					$alignment = $alignments[$index];
 
-                    $headerElement['attributes'] = [
-                        'style' => 'text-align: '.$alignment.';',
-                    ];
-                }
+					$headerElement['attributes'] = [
+						'style' => 'text-align: '.$alignment.';',
+					];
+				}
 
-                $headerElements[] = $headerElement;
-            }
+				$headerElements[] = $headerElement;
+			}
 
-            $block = [
-                'alignments'     => $alignments,
-                'identified'     => true,
-                'element'        => [
-                    'name'       => 'table',
-                    'handler'    => 'elements',
-                    'attributes' => [
-                        'class'  => 'table table-striped table-bordered table-hover'
-                    ],
-                ],
-            ];
+			$block = [
+				'alignments'     => $alignments,
+				'identified'     => true,
+				'element'        => [
+					'name'       => 'table',
+					'handler'    => 'elements',
+					'attributes' => [
+						'class'  => 'table table-striped table-bordered table-hover'
+					],
+				],
+			];
 
-            $block['element']['text'][] = [
-                'name'    => 'thead',
-                'handler' => 'elements',
-            ];
+			$block['element']['text'][] = [
+				'name'    => 'thead',
+				'handler' => 'elements',
+			];
 
-            $block['element']['text'][] = [
-                'name'    => 'tbody',
-                'handler' => 'elements',
-                'text'    => array(),
-            ];
+			$block['element']['text'][] = [
+				'name'    => 'tbody',
+				'handler' => 'elements',
+				'text'    => array(),
+			];
 
-            $block['element']['text'][0]['text'][] = [
-                'name'    => 'tr',
-                'handler' => 'elements',
-                'text'    => $headerElements,
-            ];
+			$block['element']['text'][0]['text'][] = [
+				'name'    => 'tr',
+				'handler' => 'elements',
+				'text'    => $headerElements,
+			];
 
-            return $block;
-        }
-    }
+			return $block;
+		}
+	}
 }
